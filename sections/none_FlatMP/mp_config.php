ee5cf22aa2d074a9165d958516cc569c016cdf6b
Lwangaman <donjohn.fmmi@gmail.com>
2011-06-15 04:20:31 +0200

    fix syntax of config file (see if still works correctly!)

--- sections/none_FlatMP/mp_config.php
+++ sections/none_FlatMP/mp_config.php
@@ -1,4 +1,8 @@
-<?xml version='1.0'?>
+<?php
+header('content-type: text/xml');
+exit(0);
+?>
+<<??>?xml version="1.0" encoding="UTF-8"?>
 <fm:options>
   <fm:mail>1</fm:mail>
   <fm:mexpp>25</fm:mexpp>
